68e04b6b993baa5db9b646505c44253abfedb075
Juan D <141517502+Juandrg24@users.noreply.github.com>
2024-05-03 13:38:02 -0500

    Segimiento guardar y listar validado

---
+++ BDs/juanrubio.sql
@@ -0,0 +1,93 @@
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: juanrubio
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `juanrubio`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `juanrubio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `juanrubio`;
+
+--
+-- Table structure for table `notas`
+--
+
+DROP TABLE IF EXISTS `notas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notas` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `cedula` char(10) NOT NULL,
+  `nombre` char(40) NOT NULL,
+  `nota1` float(4,2) DEFAULT NULL,
+  `nota2` float(4,2) DEFAULT NULL,
+  `nota3` float(4,2) DEFAULT NULL,
+  `definitiva` float(4,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notas`
+--
+
+LOCK TABLES `notas` WRITE;
+/*!40000 ALTER TABLE `notas` DISABLE KEYS */;
+INSERT INTO `notas` VALUES (1,'1002962946','Juan David Rubio G',5.00,4.60,5.00,4.87),(2,'1002962400','Laura Villegas',5.00,4.80,4.90,4.90);
+/*!40000 ALTER TABLE `notas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER calcular_definitiva BEFORE INSERT ON notas
+FOR EACH ROW
+BEGIN
+    DECLARE definitiva FLOAT;
+    
+    
+    SET definitiva = (NEW.nota1 + NEW.nota2 + NEW.nota3) / 3;
+
+    
+    SET NEW.definitiva = definitiva;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping routines for database 'juanrubio'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-03 13:24:04
--- Springs/Seguimiento/Notas/src/main/resources/static/validar.js
+++ Springs/Seguimiento/Notas/src/main/resources/static/validar.js
@@ -1,4 +1,83 @@
-function validaNotas(){
-    return alert("Algo");
+function validaNotas() {
+    let validado = true;
 
+    let msgs = document.querySelectorAll("small[id^='msg']");
+
+    //Limpiamos los msg con este for of
+    for (let m of msgs) {
+        m.innerHTML = "";
+    }
+
+    //Campos 
+    let cedula = document.getElementById("cedula");
+    let nombre = document.getElementById("nombre");
+    let n1 = document.getElementById("nota1");
+    let n2 = document.getElementById("nota2");
+    let n3 = document.getElementById("nota3");
+
+    //rgx
+    let rgxNombre = /^[a-zA-Z Á]{3,40}$/;
+    let rgxCedula = /^((\d{8})|(\d{10})|(\d{11})|(\d{6}-\d{5}))?$/;
+    let rgxFloat = /^\d{1,1}\.\d{1,2}$/
+
+    //msg
+
+    let msgCedula = document.querySelector("#msgCedula");
+    let msgNombre = document.querySelector("#msgNombre");
+    let msgNota1 = document.querySelector("#msgNota1");
+    let msgNota2 = document.querySelector("#msgNota2");
+    let msgNota3 = document.querySelector("#msgNota3");
+
+    //Condiciones
+
+    //Cedula
+    if (cedula.value.trim() == "" || cedula.value.lenght == 0) {
+        msgCedula.innerHTML += '<b>La Cedula No Puede Estar Vacio</b><br>';
+        validado = false;
+
+    } else if (!rgxCedula.test(cedula.value)) {
+        msgCedula.innerHTML += '<b>Formato Incorrecto Solo Números</b><br>';
+        validado = false;
+
+    }
+
+    //Nombre
+    if (nombre.value.trim() == "" || nombre.value.length == 0) {
+        msgNombre.innerHTML += '<b>El Nombre No Puede Estar Vacio</b><br>';
+        validado = false;
+    } else if (!rgxNombre.test(nombre.value)) {
+        msgNombre.innerHTML += '<b>Introduzca Solo Letras </b><br>';
+        validado = false;
+    }
+
+    //Notas 
+    //Nota1
+
+    if (n1.value.trim() == "" || n1.value.length == 0) {
+        msgNota1.innerHTML += '<b>La nota 1 No Puede Estar Vacio</b><br>';
+        validado = false;
+    } else if (!rgxFloat.test(n1.value)) {
+        msgNota1.innerHTML += '<b>Introduzca Solo Numeros o Formato Incorrecto Ej:(1.5) </b><br>'
+        validado = false;
+    }
+
+    //Nota2
+    if (n2.value.trim() == "" || n2.value.length == 0) {
+        msgNota2.innerHTML += '<b>La nota 2 No Puede Estar Vacio</b><br>';
+        validado = false;
+    } else if (!rgxFloat.test(n2.value)) {
+        msgNota2.innerHTML += '<b>Introduzca Solo Numeros o Formato Incorrecto Ej:(1.5) </b><br>'
+        validado = false;
+    }
+
+    //Nota3
+    if (n3.value.trim() == "" || n3.value.length == 0) {
+        msgNota3.innerHTML += '<b>La nota 3 No Puede Estar Vacio</b><br>';
+        validado = false;
+    } else if (!rgxFloat.test(n3.value)) {
+        msgNota3.innerHTML += '<b>Introduzca Solo Numeros o Formato Incorrecto Ej:(1.5) </b><br>';
+        validado = false;
+    }
+
+    return validado;
 }
